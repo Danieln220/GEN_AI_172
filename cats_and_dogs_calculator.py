97acb61f3e8ba28a58ab0c8e991f93287df9522a
mlg_pro220 <novizkidaniel@gmail.com>
2025-12-13 13:52:08 +0200

    Add files via upload

---
+++ cats_and_dogs_calculator.py
@@ -0,0 +1,25 @@
+
+human_Years = int(input("enter humanyears: "))
+
+cat_Years = 0
+if human_Years == 1:
+    cat_Years = 15
+elif human_Years == 2:
+    cat_Years = 15 + 9
+else: 
+    cat_Years = 15 + 9
+    remaining_years = human_Years - 2
+    cat_Years += remaining_years * 4
+
+
+dog_Years = 0
+if human_Years == 1:
+    dog_Years = 15
+elif human_Years == 2:
+    dog_Years = 15 + 9
+else: 
+    dog_Years = 15 + 9
+    remaining_years = human_Years - 2
+    dog_Years += remaining_years * 5
+
+print([human_Years, cat_Years, dog_Years])
