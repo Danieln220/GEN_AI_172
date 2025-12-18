2d8adb645351565732e822e2659aae7282e15189
Daniel <novizkidaniel@gmail.com>
2025-12-18 20:11:21 +0200

    week1day5

---
+++ week1/day5/exercise1.py
@@ -0,0 +1,16 @@
+
+sample_dict = { 
+   "class":{ 
+      "student":{ 
+         "name":"Mike",
+         "marks":{ 
+            "physics":70,
+            "history":80
+         }
+      }
+   }
+}
+
+history = sample_dict["class"]["student"]["marks"]["history"]
+print(history)
+
