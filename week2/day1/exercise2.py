6d1a1ddb8d3b202ffd6e3032bb01225858f9ab10
Daniel <novizkidaniel@gmail.com>
2025-12-22 13:38:44 +0200

    week2

--- week2/day1/exercise2.py
+++ week2/day1/exercise2.py
@@ -1,6 +1,6 @@
 #Using map and filter, try to say hello to everyone who's name is less than or equal to 4 letters
 people = ["Rick", "Morty", "Beth", "Jerry", "Snowball"]
 
-filtered_pepople = filter(lambda s: len(people) >= 4, people)
+filtered_list = filter(lambda name: len(name)<= 4, people)
+mapped_list = map(lambda name: f"hello {name}", filtered_list)
 
-print(filtered_pepople)
